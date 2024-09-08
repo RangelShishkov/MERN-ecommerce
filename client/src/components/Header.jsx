60807e6f108102fcb7850c35caceb0558c9d2a50
Rangel Shishkov <93160824+RangelShishkov@users.noreply.github.com>
2024-09-08 15:28:07 +0300

    create logout functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,8 +4,31 @@ import { GrSearch } from "react-icons/gr";
 import { FaRegUser } from "react-icons/fa6";
 import { PiShoppingCartSimple } from "react-icons/pi";
 import { Link } from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import SummaryApi from "../common";
+import {toast} from "react-toastify"
+import { setUserDetails } from "../store/userSlice";
 
 const Header = () => {
+  const user = useSelector(state => state?.user?.user)
+  const dispatch = useDispatch()
+
+  const logoutHandler = async () => {
+    const fetchData = await fetch(SummaryApi.logout_user.url,{
+      method: SummaryApi.logout_user.method,
+      credentials: 'include'
+    })
+
+    const data = await fetchData.json()
+
+    if(data.success){
+      toast.success(data.message)
+      dispatch(setUserDetails(null))
+    }
+    if(data.error){
+      toast.error(data.message)
+    }
+  }
   return (
     <header className="h-16 shadow-md bg-white">
       <div className="h-full container mx-auto flex items-center px-4 justify-between">
@@ -36,7 +59,14 @@ const Header = () => {
           </div>
 
           <div>
-            <Link to={"/login"} className="px-2 py-1 rounded-full text-white bg-cyan-300 hover:bg-cyan-500">Login</Link>
+            {
+              user?._id ? (
+                <button onClick={logoutHandler} className="px-2 py-1 rounded-full text-white bg-cyan-300 hover:bg-cyan-500">Logout</button>
+              )
+              :
+              (<Link to={"/login"} className="px-2 py-1 rounded-full text-white bg-cyan-300 hover:bg-cyan-500">Login</Link>)
+            }
+            
           </div>
         </div>
       </div>
