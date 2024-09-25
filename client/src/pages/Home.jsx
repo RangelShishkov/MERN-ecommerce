ae66698baa5cfe7e6892b999a8b6741d05fa23e3
Rangel Shishkov <93160824+RangelShishkov@users.noreply.github.com>
2024-09-25 14:08:21 +0300

    create VerticalCardProduct component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import CategoryList from "../components/CategoryList";
 import SlidingBanner from "../components/SlidingBanner";
 import HorizontalCardProduct from "../components/HorizontalCardProduct";
+import VerticalCardProduct from "../components/VerticalCardProduct";
 
 const Home = () => {
   return (
     <div>
       <CategoryList />
       <SlidingBanner />
+
       <HorizontalCardProduct category={"airpods"} heading={"Best Airpod Deals"}/>
       <HorizontalCardProduct category={"watches"} heading={"Popular Watch Deals"}/>
+
+      <VerticalCardProduct category={"phone"} heading={"Phones"} />
+      <VerticalCardProduct category={"mouse"} heading={"Mouse"} />
+      <VerticalCardProduct category={"television"} heading={"TV's"} />
+      <VerticalCardProduct category={"camera"} heading={"Camera"} />
+      <VerticalCardProduct category={"earphones"} heading={"Earphones"} />
+      <VerticalCardProduct category={"printer"} heading={"Printers"} />
+      <VerticalCardProduct category={"processor"} heading={"CPU's"} />
+      <VerticalCardProduct category={"speaker"} heading={"Speakers"} />
+      <VerticalCardProduct category={"refrigerator"} heading={"Refrigerators"} />
+      <VerticalCardProduct category={"trimmer"} heading={"Trimmers"} />
     </div>
   );
 };
